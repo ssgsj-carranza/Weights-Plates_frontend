d5536d324e3fdf3994b4bced4a1dc10720b098f3
unknown <ssgsj.carranza@gmail.com>
2021-05-27 15:05:25 -0500

    finished work on the footer, working on sigin page next

--- weights-plates-frontend/src/components/Footer/index.js
+++ weights-plates-frontend/src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaSnapchat } from 'react-icons/fa';
 import {FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTiltle, FooterLink} from './FooterElements';
-
+import {SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './FooterElements';
 const Footer = () => {
     return (
         <FooterContainer>
@@ -9,28 +10,41 @@ const Footer = () => {
                 <FooterLinksWrapper>
                     <FooterLinkItems>
                         <FooterLinkTiltle>About Us</FooterLinkTiltle>
-                            <FooterLink to='/signin'>Contact Us</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
                     </FooterLinkItems>
                     <FooterLinkItems>
-                        <FooterLinkTiltle>About Us</FooterLinkTiltle>
-                            <FooterLink to='/signin'>Contact Us</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        <FooterLinkTiltle>Contact Us</FooterLinkTiltle>
+                            <FooterLink to='/'>Contact</FooterLink>
+                            <FooterLink to='/'>Support</FooterLink>
                     </FooterLinkItems>
                 </FooterLinksWrapper>
                 <FooterLinksWrapper>
                     <FooterLinkItems>
-                        <FooterLinkTiltle>About Us</FooterLinkTiltle>
-                            <FooterLink to='/signin'>Contact Us</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTiltle>About Us</FooterLinkTiltle>
-                            <FooterLink to='/signin'>Contact Us</FooterLink>
-                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        <FooterLinkTiltle>Social Media</FooterLinkTiltle>
+                            <FooterLink to='/'>Facebook</FooterLink>
+                            <FooterLink to='/'>Instagram</FooterLink>
+                            <FooterLink to='/'>Snapchat</FooterLink>
                     </FooterLinkItems>
                 </FooterLinksWrapper>
               </FooterLinksContainer>
+              <SocialMedia>
+                  <SocialMediaWrap>
+                      <SocialLogo to='/'>Weights and Plates</SocialLogo>
+                      <WebsiteRights>Weights and Plates © {new Date().getFullYear()}All rights reserved</WebsiteRights>
+                      <SocialIcons>
+                          <SocialIconLink href='//www.facebook.com/jorge.r.carranza.3/' target='_blank' aria-label='Facebook'>
+                              <FaFacebook/>
+                          </SocialIconLink>
+                          <SocialIconLink href='//www.https://www.instagram.com/laidpidgeon/' target='_blank' aria-label='Instagram'>
+                              <FaInstagram/>
+                          </SocialIconLink>
+                          <SocialIconLink href='/' target='_blank' aria-label='Snapchat'>
+                              <FaSnapchat/>
+                          </SocialIconLink>
+                      </SocialIcons>
+                  </SocialMediaWrap>
+              </SocialMedia>
             </FooterWrap>  
         </FooterContainer>
     )
