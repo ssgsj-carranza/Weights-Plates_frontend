173bd81ab782126e15bd670bb8be4fbf5d8cf68c
unknown <ssgsj.carranza@gmail.com>
2021-05-27 12:43:00 -0500

    using react scroll to make UX better, with each section having a link to the corresponding route

---
+++ weights-plates-frontend/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDescription: true,
+    topLine: 'The Only Nutrition Database You Will Ever Need',
+    headLine: 'Special Diets/Intolerances & Essential Supplements with Nutitional Information',
+    descriptions: 'We compute the nutritional information for recipes automatically using a proprietary algorithm. With this information, you can find individual recipes or even create entire meal plans that satisfy your dietary goals.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/jpg-1.jpg'),
+    alt: 'Food',
+    dark: true,
+    primary: true,
+    darkText: false
+}
