7831bb346af03528fd05d9712816eb6e8a3fc147
unknown <ssgsj.carranza@gmail.com>
2021-05-28 12:42:42 -0500

    added file to contain all my axios requests, all end points tested 200 ok

---
+++ weights-plates-frontend/src/components/APIcalls/Api.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+export async function registerService(user){
+    try{
+        const response = await axios.post('https://localhost:44394/api/register', user);
+        if (response.status === 201){
+            console.log('You are registered');
+            return response.data;
+        }
+    }
+    catch(ex){
+        console.log('Error', ex);
+        throw ex;
+    }
+}
+
+export async function loginService(login){
+    try{
+        const response = await axios.post('http://127.0.0.1:8000/api/login/', login);
+        console.log('You are logged in');
+        return response.data;
+    }
+    catch(ex){
+        console.log('Error');
+        throw ex;
+    }
+}
+
+export async function getCurrentUser(){
+    try{
+        const jwt = localStorage.getItem('token');
+        const response = await axios.get(`http://127.0.0.1:8000/user/${id}`, {headers: {Authorization: 'Token' + jwt}});
+        console.log(response.data);
+
+        if(response.status === 200){
+            return response.data;
+        }
+        return null;
+    }
+    catch(ex){
+        console.log("exception", ex)
+    }
+}
+
+export async function getSupplements(){
+    const response = await axios.get('http://127.0.0.1:8000/supplements');
+    console.log(response.data);
+    return(response.data)
+}
+
